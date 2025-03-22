f2e913943254361a33650fcbe385d5bf2801a116
Stew <itsstew2002@gmail.com>
2025-03-22 13:03:54 +0545

    Added functions for validating chain and resolving conflicts\

--- blockchain.py
+++ blockchain.py
@@ -20,6 +20,49 @@ class Blockchain:
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f"{last_block}")
+            print(f"{block}")
+            print("\n--------------\n")
+
+            if block["previous_hash"] != self.hash(last_block):
+                return False
+
+            if not self.valid_proof(
+                last_proof=last_block["proof"],
+                proof=block["proof"],
+            ):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = request.get(f"http://{node}/chain")
+            if response.status_code == 200:
+                length = response.json()["length"]
+                chain = response.json()["chain"]
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
     def new_block(self, proof, previous_hash=None):
         block = {
             "index": len(self.chain) + 1,
@@ -119,6 +162,7 @@ def full_chain():
         "length": len(blockchain.chain),
     }
     return jsonify(response), 200
+
 
 
 if __name__ == "__main__":
